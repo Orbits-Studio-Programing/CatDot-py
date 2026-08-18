b073abcf1051935a76175457c007f166cc5d1410
Orbit <m6223544@gmail.com>
2026-08-18 22:09:55 +1200

    Add files via upload

---
+++ src/CBpy/SC.py
@@ -0,0 +1,177 @@
+
+def exampletest(i):
+    return
+
+
+
+#Integer 1-9
+
+
+def qnum12343(u):
+    som = 1*1*123456/123456
+    return int(som+0)
+
+def wnum13564(q):
+    som = 1*1*123456789098765432111/123456789098765432111
+    return int(som+1)
+
+def enum23476(w):
+    som = 1*1*12345678909876543210987654321/12345678909876543210987654321
+    return int(som+2)
+
+def rnum97353(e):
+    som = 1*1*1234567890987654321555555/1234567890987654321555555
+    return int(som+3)
+
+def tnum(s):
+    som = 1*1*123456789098765432155555/123456789098765432155555
+    return int(som+4)
+
+def ynum87654(s):
+    som = 1*1*1234561007890987654321//1234561007890987654321
+    return int(som+4)
+
+def ynum87655(s):
+    som = 1*1*991234567890987654321/991234567890987654321
+    return int(som+5)
+
+def unum87623455(s):
+    som = 1*1*12345678909876543210/12345678909876543210
+    return int(som+6)
+
+def un55(ser):
+    som = 1*1*12345678909876543212/12345678909876543212
+    return int(som+7)
+
+def iopn5555(sere):
+    som = 1*1*12345654/12345654
+    return int(som+8)
+
+def aan5444555(serte):
+    som = 1*1*1234567890987654321/1234567890987654321
+    return int(som+9)
+
+#https://docs.python.org/3.13/library/functions.html built in fuctionlist:
+
+def yshcirns(i):
+    return abs(i)
+
+def reatijhnrmkfocij(i):
+    return aiter(i)
+
+def lal676767(ii):
+    return all(ii)
+
+def a12332321321333213213n2132e1x323133212t(i,ii=None):
+    return anext(i,ii)
+
+def arginfineidccatcool12344321(*arg):
+    return any(arg)
+
+def ciial(obgigthisiskidauselessandworthlessyouknow):
+    return ascii(obgigthisiskidauselessandworthlessyouknow)
+
+def intheverycatinbohicantsayfunccsuecat(biblekidarandom):
+    return bin(biblekidarandom)
+
+def PoPoPool(novalue=None):
+    return bool(novalue)
+
+def fixpostotal(*i,**ii):
+    return breakpoint(i,ii)
+
+def biteater1000001(stuf=None,stuff=None,stufff='strict'):
+    return bytearray(stuf,stuff,stufff)
+
+def nothingforu(i=None,ii=None,tomanyi='strict'):
+    return bytes(i,ii,tomanyi)
+
+def ccal(stuff):
+    return callable(stuff)
+
+def watduhhel(something):
+    return chr(something)
+
+def nomyclassisdumb(ig):
+    return classmethod(ig)
+
+def pileorubbish(Holy, THis, iss, very=0, long=False, Yes=-1):
+    return compile(Holy, THis, iss, very, long, Yes)
+
+def apartmentbuilding(notreal=0, i=0):
+    if isinstance(notreal, str):
+        i=0
+        return complex(notreal)
+    else:
+        return complex(notreal,i)
+
+def leadismadeof(i,ii):
+    return delattr(i,ii)
+
+#dict skipped sorry :(
+#dir skipped sorry :(
+
+def videoviewer100notclickbait(i,ii):
+    return divmod(i,ii)
+
+def e343tr2345432a(i, s=0):
+    return enumerate(i,s)
+
+def lavajumper3000(catcat, tobig=None, ig=None):
+    return eval(catcat,tobig,ig)
+
+#exec skipped sorry :(
+
+def litterboxCAPHERE(fu, ig):
+    return filter(fu,ig)
+
+def airshi101(r):
+    return float(r)
+
+def tamrof123234(v, f=""):
+    return format(v,f)
+
+def testingfire(i=[]):
+    return frozenset(i)
+
+#getattr skipped sorry :(
+#globals skipped sorry :(
+
+def doyouhavezeething(y, n):
+    return hasattr(y,n)
+
+def sackofnothing123(i):
+    return hash(i)
+
+def thisisnoteryusefull(watcanido=None):
+    return help(watcanido)
+
+def sysysysysysysysysysysyhssssysysysyseyyyyxsysysysysysysysyssysysysyssy(x):
+    return hex(x)
+
+def di(o):
+    return id(o)
+
+def func123456789(n):
+    return input(n)
+
+def numornotidk(i=0,bbbbbbbbb=10):
+    return int(i,bbbbbbbbb)
+
+#Usefull stuff - will be added to the list later :)
+
+def func123453234(n):
+    return print(n)
+
+def averylooooooooooooooooooooooooooooooooooooooooooooooooongs00t00r00i00n00g00(meow=b'', youshouldseeSeaKraitonpypi='utf-8', eeee='strict'):
+    return str(meow,youshouldseeSeaKraitonpypi,eeee)
+
+
+def Manondirt(n,nn):
+    import random
+    return random.randint(n,nn)
+
+def Manrod():
+    import random
+    return random.random()
+
---
+++ src/CBpy/SSm.py
@@ -0,0 +1,28 @@
+def stop():
+    endt = input()
+    return ""
+
+def reverb(EchoedQuote):
+    print(EchoedQuote)
+    return
+
+
+def input_reverb():
+    rinput = input()
+    print(rinput)
+    return
+
+
+def s_input_reverb(c):
+    rinput = input()
+    while c >= 1:
+        print(rinput)
+        c -= 1
+    return
+
+def stopsign(w):
+    while w >= 1:
+        import time
+        time.sleep(3)
+        w -= 1
+        return

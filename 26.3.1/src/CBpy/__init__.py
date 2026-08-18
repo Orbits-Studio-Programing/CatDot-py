4c5535be26711d11d50b386d22506fff9f4f6766
Orbit <m6223544@gmail.com>
2026-08-18 21:52:20 +1200

    Add files via upload

---
+++ 26.3.1/src/CBpy/__init__.py
@@ -0,0 +1,15 @@
+from . import CD
+from . import SC
+from . import seaci
+from . import seagrass
+from . import seahi
+from . import seamath
+from . import seaprint
+from . import seatime
+from . import seaweed
+from . import SSm
+
+
+__all__ = ["CD", "SC", "seaci", "seagrass", "seahi", "seamath", "seaprint", "seatime", "seaweed", "SSm"]
+
+__VERSION__ = "26.3.1"

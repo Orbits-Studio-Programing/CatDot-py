adbd2b9e45d2ad66a1097304a69fe23882587b13
Orbit <m6223544@gmail.com>
2026-08-20 19:58:16 +1200

    Delete src/CBpy directory

--- src/CBpy/Archive/__init__.py
+++
@@ -1,6 +0,0 @@
-from . import Seacret
-
-
-__all__ = ["Seacret"]
-
-Archive_VER = "26.1.1"
--- src/CBpy/__init__.py
+++
@@ -1,9 +0,0 @@
-from . import CrackDown
-from . import SeaKrait
-from . import StopSign
-from . import Archive
-
-
-__all__ = ["CrackDown", "StopSign", "Archive", "SeaKrait"]
-
-__VERSION__ = "26.4.0"
